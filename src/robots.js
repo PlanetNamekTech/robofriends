88911657148384e7c89f148587bbdaa9a6d696ad
Spencer Richardson <sr23513@gmail.com>
2022-05-31 16:04:57 -0700

    initial UI components

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Isaac Newton',
+    username: 'SirIsaac',
+    email: 'SirNewton@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Inego Montoya',
+    username: 'scarface',
+    email: 'scarface@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Rafael Turtle',
+    username: 'Raf',
+    email: 'Raffi@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Candy Horton',
+    username: 'CandyHorton',
+    email: 'Candy_Horton@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Dwaye White',
+    username: 'IalreadyDidthat',
+    email: 'Dwhite68@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Elmer Fudd',
+    username: 'WhatsUpDoc',
+    email: 'Elmer_pest_control@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Mohammed El-Arian',
+    username: 'Tha_economist',
+    email: 'Legos7@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Tom Riddle',
+    username: 'The_Riddler',
+    email: 'Riddes@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'James Jameson',
+    username: 'JamesJames',
+    email: 'Jjameson@gmail.com'
+  },
+]
